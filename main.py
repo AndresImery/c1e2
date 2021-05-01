a693643cb1699cb6a7fc1233ece279e46cee2584
Andres Imery <andresimery@gmail.com>
2021-05-01 03:48:42 -0400

    Tarea c1e1 lista

---
+++ main.py
@@ -0,0 +1,5 @@
+name = input("Ingrese su nombre: ")
+age = input("Ingrese su edad: ")
+major = input("Ingrese su carrera de estudio: ")
+
+print(f"Hola Mundo! mi nombre es {name} y tengo {age} años, estudio {major} en la UNIMET!")
